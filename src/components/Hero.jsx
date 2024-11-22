5aec90181bbf6f80a26d05482adec211814bb51d
iambrasco <ebangabdon641@gmail.com>
2024-11-22 19:53:46 +0900

    o

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
       <div className='container mx-auto flex flex-col md:flex-row items-center justify-between p-8 overflow-y-hidden gap-12 h-full'>
         {/* left side */}
         <motion.div className='md:w-1/2' variants={fadeIn('down', 0.2)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}}>
-            <h1 className='text-4xl font-secondary font-bold mb-4 md:w-3/5 leading-snug'>Conférence à venir sur l'IA</h1>
+            <h1 className='text-4xl font-secondary font-bold mb-4 md:w-3/5 leading-snug'>Conférence sur l'IA</h1>
             <p className='text-xl font-extralight mb-12 md:pr-8'>Le centre annonce la tenue prochaine d'une conférence sur les applications de  l'intelligence artificielle en Science.
               Pour Celles et Ceux qui souhaitent exposer, un formulaire est disponible afin de fournir les informations de l'exposé.
             </p>
             <button className='bg-primary text-[#FFF8DC] py-3.5 px-8 font-light rounded-md hover:bg-primary/90'>
-              <a href="#contact" className='flex gap-1 items-center'>
-                <span>En Savoir plus</span>
+              <a href="https://meet.google.com/fxu-pzfy-nqv" className='flex gap-1 items-center'>
+                <span>Lien google meet</span>
               <IoArrowForwardCircleSharp />
                 </a>
             </button>
