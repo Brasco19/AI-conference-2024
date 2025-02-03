b6d836b221fe6119df684682f74db1aecefe7838
Brasco19 <137790824+Brasco19@users.noreply.github.com>
2025-02-04 02:00:46 +0900

    Update Infos.jsx

--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -50,14 +50,10 @@ const Infos = () => {
               <motion.div 
               variants={fadeIn('up', 0.2)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}}
                 className='flex flex-col items-center justify-center bg-[#e5eaee] rounded-lg p-12 font-secondary mx-auto'>
-                <h5 className='text-3xl font-semibold text-heroBg mb-4 items-center'>Analyse des Données Relatives à la Tuberculose au Gabon avec R </h5>
+                <h5 className='text-3xl font-semibold text-heroBg mb-4 items-center'>Pas disponible pour le moment! </h5>
                 <p className="">
-                  <span className='font-bold'>Résume: </span> Selon les estimations de l'Organisation mondial de la santé (OMS), au Gabon 
-                  l'incidence de la tuberculose (TB) toutes formes et des rechutes était de 521 cas pour 100 000 habitant.e.s en 2019. 
-                  Au cours de cette année, le Programme national de lutte contre la tuberculose a déclaré 5399 nouveaux 
-                  cas et rechutes pour un total de cas déclarés de 5573.
-
-                  Nous allons présenter un aperçu de l'évolution de la tuberculose au Gabon entre 2018 et 2022, en utilisant les données disponibles que nous avons.
+                  <span className='font-bold'>Résume: </span>  
+                  Pas disponible pour le moment!
                 </p>
               </motion.div>
             </TabPanel>
@@ -66,17 +62,11 @@ const Infos = () => {
               <motion.div 
               variants={fadeIn('up', 0.2)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}}
                 className='flex flex-col items-center justify-center bg-[#e5eaee] rounded-lg p-12 font-secondary mx-auto'>   
-                <h5 className='text-2xl font-semibold text-heroBg mb-4 items-center'>Comprendre les Modèles d'Intelligence Artificielle Générative: Cas du Modèle de Diffusion </h5>
+                <h5 className='text-2xl font-semibold text-heroBg mb-4 items-center'>Pas disponible pour le moment!</h5>
                 <p className="">
                   <span className='font-bold'>Résume: </span>
-                  L’IA générative se distingue par sa capacité à créer et à innover, par opposition à la simple analyse ou à la classification. 
-                  Les progrès dans ce domaine ont conduit à des percées dans des domaines créatifs et des applications pratiques, 
-                  ce qui en fait un domaine de pointe de la recherche et du développement de l’IA.
-
-                  Depuis 2024, les modèles de diffusion sont principalement utilisés pour les tâches de vision par ordinateur, notamment le débruitage d'images,
-                  l'inpainting, la super-résolution, la génération d'images et la génération de vidéos. 
-                  Celles-ci impliquent généralement la formation d'un réseau neuronal pour débruiter séquentiellement les images floues avec le bruit gaussien.
-                </p>
+                  Pas disponible pour le moment!
+                  </p>
               </motion.div>
             </TabPanel>
 
